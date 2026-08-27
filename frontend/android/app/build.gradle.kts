73883c5b13c881712f52787f49b06782cb32cddc
aaryankasaudhan91-dev <aaryankasaudhan91@gmail.com>
2026-08-27 17:07:25 +0530

    feat: initialize Android build configuration with custom build directory and SDK 36 settings

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.project_parakh_mobile"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
