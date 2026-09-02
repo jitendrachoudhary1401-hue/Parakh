944b80bdc1c2567961496fd17883974e7a5499e7
aaryankasaudhan91-dev <aaryankasaudhan91@gmail.com>
2026-09-03 00:33:52 +0530

    fix(android): update app icon, set package name to gov.doca.parakh, configure wireless backend connectivity and resolve video player lifecycle

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "in.gov.doca.parakh"
+    namespace = "gov.doca.parakh"
     compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
@@ -15,7 +15,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "in.gov.doca.parakh"
+        applicationId = "gov.doca.parakh"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- frontend/android/app/src/main/kotlin/gov/doca/parakh/MainActivity.kt
+++ frontend/android/app/src/main/kotlin/gov/doca/parakh/MainActivity.kt
@@ -1,6 +1,5 @@
-package com.example.project_parakh_mobile
+package gov.doca.parakh
 
 import io.flutter.embedding.android.FlutterFragmentActivity
 
 class MainActivity : FlutterFragmentActivity()
-
